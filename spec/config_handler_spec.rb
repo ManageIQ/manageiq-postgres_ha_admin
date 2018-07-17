1ea07e0cb0c90e4eed5d9c7a709b77a33519b61d
Nick Carboni <ncarboni@redhat.com>
2018-07-17 13:38:29 -0400

    Add before and after failover callbacks to ConfigHandler
    The failover monitor will call these at appropriate times when
    executing a failover for a particular config source.

---
+++ spec/config_handler_spec.rb
@@ -0,0 +1,47 @@
+describe ManageIQ::PostgresHaAdmin::ConfigHandler do
+  describe "#before_failover" do
+    it "raises an ArgumentError if called without a block" do
+      expect { subject.before_failover }.to raise_error(ArgumentError)
+    end
+  end
+
+  describe "#after_failover" do
+    it "raises an ArgumentError if called without a block" do
+      expect { subject.after_failover }.to raise_error(ArgumentError)
+    end
+  end
+
+  describe "#do_before_failover" do
+    it "runs with no callback registered" do
+      subject.do_before_failover
+    end
+
+    it "calls the registered failover" do
+      before_failover_obj = double("before_failover_object")
+
+      subject.before_failover do
+        before_failover_obj.before_failover_things
+      end
+
+      expect(before_failover_obj).to receive(:before_failover_things)
+      subject.do_before_failover
+    end
+  end
+
+  describe "#do_after_failover" do
+    it "runs with no callback registered" do
+      subject.do_after_failover
+    end
+
+    it "calls the registered failover" do
+      after_failover_obj = double("after_failover_object")
+
+      subject.after_failover do
+        after_failover_obj.after_failover_things
+      end
+
+      expect(after_failover_obj).to receive(:after_failover_things)
+      subject.do_after_failover
+    end
+  end
+end
